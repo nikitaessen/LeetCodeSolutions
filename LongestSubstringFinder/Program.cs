be980de3af58129d2f611817154bb8bb94a2f17e
Nick Essen <nikitaessen@gmail.com>
2018-09-10 13:31:11 +0300

    Simple solution, w/o optimizations

--- LongestSubstringFinder/Program.cs
+++ LongestSubstringFinder/Program.cs
@@ -1,15 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LongestSubstringFinder
+﻿namespace LongestSubstringFinder
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			var sol = new Solution();
+
+			var result = sol.LengthOfLongestSubstring("ubsvutpwmu");
+		}
+	}
+
+	public class Solution
+	{
+		public int LengthOfLongestSubstring(string s)
+		{
+			var longest = string.Empty;
+			var buff = string.Empty;
+
+			for (var i = 0; i < s.Length; i++)
+			{
+				if (buff.Contains(s[i].ToString()))
+				{
+					if (buff.Length > longest.Length)
+					{
+						longest = string.Copy(buff);
+					}
+
+					var j = buff.Length - 1;
+
+					for(;;)
+					{
+						if (buff[j] == s[i])
+						{
+							buff = buff.Remove(0, j + 1);
+							buff = $"{buff}{s[i]}";
+							break;
+						}
+
+						j--;
+					}
+				}
+				else
+				{
+					buff += s[i];
+				}
+			}
+
+			if (buff.Length > longest.Length)
+			{
+				longest = string.Copy(buff);
+			}
+
+			return longest.Length;
 		}
 	}
 }

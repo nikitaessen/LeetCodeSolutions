59878f04c0d788c22ed8ecd6d0ba61eff5eb86b4
Nick Essen <nikitaessen@gmail.com>
2018-09-11 13:56:48 +0300

    Simple solution throuth char array

---
+++ ReverseInteger/IntegerExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace ReverseInteger
+{
+	public class IntegerExtensions
+	{
+		private const char NEGATIVE_SIGN = '-';
+
+		public int Reverse(int x)
+		{
+			var arr = x.ToString().ToCharArray();
+
+			var invertedArr = arr.Reverse().ToArray();
+
+			var str = new string(invertedArr);
+
+			if (str.Last() == NEGATIVE_SIGN)
+			{
+				str = str.Remove(str.Length - 1);
+				str = $"{NEGATIVE_SIGN}{str}";
+			}
+
+			return int.TryParse(str, out var result) ? result : 0;
+		}
+	}
+}
---
+++ ReverseInteger/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseInteger
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var ext = new IntegerExtensions();
+
+			var res = ext.Reverse(1534236469);
+		}
+	}
+}

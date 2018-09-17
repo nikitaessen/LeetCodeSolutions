4bae5a2c0210907c6887e10d62998e83edce1910
Nick Essen <nikitaessen@gmail.com>
2018-09-17 12:33:17 +0300

    Slow solution

---
+++ LongestPalindrome/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LongestPalindrome
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var ext = new StringExtensions();
+
+			var res = ext.LongestPalindrome("aacdefcaa");
+		}
+	}
+}
---
+++ LongestPalindrome/StringExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+
+namespace LongestPalindrome
+{
+	public class StringExtensions
+	{
+		public string LongestPalindrome(string s)
+		{
+			if (string.IsNullOrWhiteSpace(s))
+			{
+				return string.Empty;
+			}
+
+			var n = s.Length;
+
+			var table = new bool[n, n];
+
+			var maxLength = 1;
+
+			for (var i = 0; i < n; i++)
+			{
+				table[i, i] = true;
+			}
+
+			var startIndex = 0;
+			for (var i = 0; i < n - 1; ++i)
+			{
+				if (s.ElementAt(i) == s.ElementAt(i + 1))
+				{
+					table[i, i + 1] = true;
+					startIndex = i;
+					maxLength = 2;
+				}
+			}
+
+			for (var k = 3; k <= n; ++k)
+			{
+				for (var i = 0; i < n-k+1; ++i)
+				{
+					var j = i + k - 1;
+
+					if (table[i + 1, j - 1] && s.ElementAt(i) == s.ElementAt(j))
+					{
+						table[i, j] = true;
+
+						if (k > maxLength)
+						{
+							startIndex = i;
+							maxLength = k;
+						}
+					}
+				}
+			}
+
+			return s.Substring(startIndex, maxLength);
+		}
+	}
+}
